2db2bf7967659977485e547fec0b80a0a55f18c5
AJ Phoenix <ajph03nix@gmail.com>
2022-08-07 23:10:37 -0400

    Project buttons fetch project info from json file(only logged)

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 
+
 const modal = document.querySelector(".modal");
+const infoModal = document.querySelector(".infoModal");
 const triggers = document.querySelectorAll(".trigger");
+const infoTriggers = document.querySelectorAll(".infoTrigger");
 // const trigger2 = document.querySelectorAll(".trigger2");
 const closeButton = document.querySelector(".close-button");
+const infoCloseButton = document.querySelector(".infoClose-button");
+let projects = []
+let currentInfo = {}
 // const frameContent = document.querySelector("")
 
-const SubEarthURL =  "https://ajphnx.github.io/SubEarth/"
+
+const SubEarthURL = 'https://ajphnx.github.io/SubEarth/'
 const nVentoryURL = "https://nventory-frontend.herokuapp.com/"
 const afterClassURL = "./assets/AfterClass_Mockup_05_alt_logo.png"
 
-function clearModal (){
-    document.getElementById('projectFrame').src = '';
+
+function clearModal (id){
+    document.getElementById(id).src = '';
 }
 
 function changeSrc(loc) {
@@ -27,6 +35,9 @@ function changeSrc(loc) {
 function toggleModal() {
     modal.classList.toggle("show-modal");
 }
+function toggleInfoModal() {
+    modal.classList.toggle("show-InfoModal");
+}
 
 function windowOnClick(event) {
     if (event.target === modal) {
@@ -34,10 +45,11 @@ function windowOnClick(event) {
     }
 }
 
-function fillFrame(id){
-    switch(id) {
+function fillFrame(name){
+    switch(name) {
         case "subEarth":
             changeSrc(SubEarthURL)
+
           break;
         case "nVentory":
             changeSrc(nVentoryURL)
@@ -46,16 +58,38 @@ function fillFrame(id){
             changeSrc(afterClassURL)
           break;
     }
+    fillInfo(name);
 }
-
-triggers.forEach(trigger =>{
+//Fetch info from Json file
+async function fetchInfo(){
+    const response = await fetch('./assets/Projects.json')
+    const data = await response.json()
+    projects = data.projects
+    // projects.forEach(project => {
+        
+    //     console.log(project.name,': ', project.description)
+    // })
+    // document.getElementById('infoWrap').textContent = name;
+}
+// Fill json info based on selected project button
+function fillInfo(name){
+    currentInfo = projects.find(project =>{
+        return (
+            project.name === name
+        )
+    })
+    console.log(currentInfo)
+}
+triggers.forEach(trigger =>{    
     trigger.addEventListener("click",function(e){
     console.log(trigger.id);
-    clearModal ();
+    clearModal ('projectFrame');
     fillFrame(trigger.id);
     // fillFrame(e.target.id);
     });
 });
-
+// infoTriggers.addEventListener("click",toggleInfoModal)
 closeButton.addEventListener("click", toggleModal);
+// infoCloseButton.addEventListener("click", toggleInfoModal);
+window.addEventListener("load",fetchInfo)
 window.addEventListener("click", windowOnClick);
